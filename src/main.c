c49e2330b5861dc11dc610f304cabc03604fec39
Yhya Ibrahim <yhyamobile57@gmail.com>
2026-04-04 20:31:29 +0200

    inline term.h into main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,72 @@
 #include <ctype.h>
+#include <malloc.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TERM_IMPLEMENTATION
-#include "term.h"
+#include <termios.h>
+#include <unistd.h>
+
+void write_into_screen(const char *cmd) {
+    write(STDOUT_FILENO, cmd, strlen(cmd));
+}
+
+static struct termios original_termios;
+
+void enter_raw_mode(void) {
+    tcgetattr(STDIN_FILENO, &original_termios);
+
+    struct termios raw = original_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 0;
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    write_into_screen("\x1b[?1049h\x1b[2J\x1b[H");
+}
+
+uint8_t read_key(void) {
+    uint8_t c;
+
+    if (read(STDIN_FILENO, &c, 1)) {
+        return c;
+    }
+
+    return 0;
+}
+
+void exit_raw_mode(void) {
+    write_into_screen("\x1b[?1049l");
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void clear_screen(void) { write_into_screen("\x1b[2J\x1b[H"); }
+
+void draw_into_screen(void) {
+    // Do nothing for now
+}
+
+void process_keys(void) {
+    switch (read_key()) {
+    case CTRL('q'):
+        clear_screen();
+        exit_raw_mode();
+        exit(0);
+        break;
+    }
+}
 
 int main(void) {
-    Term term = term_open();
+    enter_raw_mode();
 
     for (;;) {
-        switch (term_read()) {
-        case CTRL('q'):
-            term_close(&term);
-            exit(0);
-            break;
-        }
+        clear_screen();
+        draw_into_screen();
+        process_keys();
     }
 }

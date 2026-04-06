cc96e08c68f60b003754a376240be8340c4d4635
Yhya Ibrahim <yhyamobile57@gmail.com>
2026-04-06 10:42:44 +0200

    remove theme.c and put it into theme.h instead

--- src/theme.h
+++ src/theme.h
@@ -14,4 +14,11 @@ typedef struct {
     Color statusline_foreground;
 } Theme;
 
-Theme kanagawa_wave(void);
+static inline Theme kanagawa_wave(void) {
+    return (Theme){
+        .editor_background = (Color){0x1f, 0x1f, 0x28},
+        .editor_foreground = (Color){0xdc, 0xd7, 0xba},
+        .statusline_background = (Color){0x2a, 0x2a, 0x37},
+        .statusline_foreground = (Color){0xc8, 0xc0, 0x93},
+    };
+}

cbe71c446076a2713b576bac4ebc583695554276
Yhya Ibrahim <yhyamobile57@gmail.com>
2026-04-06 11:21:27 +0200

    handle cursor movement keys

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <malloc.h>
 #include <stdbool.h>
@@ -10,16 +11,28 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define ESC 27
+
 #define BUFFER_MAX 4096
 
+typedef enum {
+    NORMAL_MODE,
+    INSERT_MODE,
+} QalamMode;
+
 typedef struct {
-    struct termios original_termios;
+    QalamMode mode;
+
+    int cursor_x;
+    int cursor_y;
 
     int screen_rows;
     int screen_cols;
 
     uint8_t buffer[BUFFER_MAX];
     size_t buffer_len;
+
+    struct termios original_termios;
 } Qalam;
 
 static Qalam q;
@@ -75,6 +88,12 @@ void exit_raw_mode(void) {
 
 void clear_screen(void) { append("\x1b[2J\x1b[H"); }
 
+void draw_cursor(void) {
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", q.cursor_y + 1, q.cursor_x + 1);
+    append_with_len(buf, strlen(buf));
+}
+
 void draw_editor_rows(void) {
     for (int y = 0; y < q.screen_rows; y++) {
         append("~");
@@ -91,7 +110,7 @@ void refresh_screen(void) {
     append("\x1b[?25l");
     append("\x1b[H");
     draw_editor_rows();
-    append("\x1b[H");
+    draw_cursor();
     append("\x1b[?25h");
     flush();
 }
@@ -157,13 +176,131 @@ void handle_sigwinch(int sig) {
     }
 }
 
-void process_keys(void) {
-    switch (read_key()) {
+typedef enum {
+    ARROW_UP,
+    ARROW_DOWN,
+    ARROW_LEFT,
+    ARROW_RIGHT,
+    NOT_AN_ARROW,
+} ArrowKey;
+
+ArrowKey read_arrow(uint8_t c) {
+    if (c == '\x1b') {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+            case 'A':
+                return ARROW_UP;
+            case 'B':
+                return ARROW_DOWN;
+            case 'C':
+                return ARROW_RIGHT;
+            case 'D':
+                return ARROW_LEFT;
+            }
+        }
+    }
+
+    return NOT_AN_ARROW;
+}
+
+void process_keys_in_insert_mode(uint8_t c) {
+    switch (c) {
+    case ESC:
+    case CTRL('c'):
+        q.mode = NORMAL_MODE;
+        break;
+    }
+}
+
+void move_cursor(int x_off, int y_off) {
+    int x_new = q.cursor_x + x_off;
+    int y_new = q.cursor_y + y_off;
+
+    if (x_new >= 0 && x_new < q.screen_cols) {
+        q.cursor_x = x_new;
+    }
+
+    if (y_new >= 0 && y_new < q.screen_rows) {
+        q.cursor_y = y_new;
+    }
+}
+
+void process_keys_in_normal_mode(uint8_t c) {
+    switch (c) {
+    case 'j':
+        move_cursor(0, +1);
+        break;
+
+    case 'k':
+        move_cursor(0, -1);
+        break;
+
+    case 'h':
+        move_cursor(-1, 0);
+        break;
+
+    case 'l':
+        move_cursor(+1, 0);
+        break;
+
+    case 'i':
+        q.mode = INSERT_MODE;
+        break;
+
     case CTRL('q'):
         clear_screen();
         flush();
         exit_raw_mode();
         exit(0);
+        break;
+    }
+}
+
+void process_keys(void) {
+    uint8_t c = read_key();
+
+    switch (read_arrow(c)) {
+    case ARROW_UP:
+        move_cursor(0, -1);
+        c = read_key();
+        break;
+
+    case ARROW_DOWN:
+        move_cursor(0, +1);
+        c = read_key();
+        break;
+
+    case ARROW_LEFT:
+        move_cursor(-1, 0);
+        c = read_key();
+        break;
+
+    case ARROW_RIGHT:
+        move_cursor(+1, 0);
+        c = read_key();
+        break;
+
+    case NOT_AN_ARROW:
+    default:
+        break;
+    }
+
+    switch (q.mode) {
+    case NORMAL_MODE:
+        process_keys_in_normal_mode(c);
+        break;
+
+    case INSERT_MODE:
+        process_keys_in_insert_mode(c);
+        break;
+
+    default:
+        assert(false && "UNREACHABLE");
         break;
     }
 }
@@ -176,6 +313,10 @@ int main(void) {
     }
 
     signal(SIGWINCH, handle_sigwinch);
+
+    q.mode = NORMAL_MODE;
+    q.cursor_x = 0;
+    q.cursor_y = 0;
 
     for (;;) {
         refresh_screen();
